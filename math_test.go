a9cfd55f05464151cec1ca512537b32973c81010
Luam <luamleiverton@gmail.com>
2022-07-13 21:54:50 -0300

    fix: correcting argument in test

--- math_test.go
+++ math_test.go
@@ -2,10 +2,9 @@ package main
 
 import "testing"
 
-func TesteSoma(t *testing.T) {
-	total := Soma(15+15)
+func TestSoma(t *testing.T) {
+	total := Soma(15, 15)
 	if total != 30 {
-		t.Errorf("Resultado da soma inválido %d: Esperado = %d",
-		total, 30)
+		t.Errorf("Resultado da soma inválido %d. Esperado = %d", total, 30)
 	}
 }
